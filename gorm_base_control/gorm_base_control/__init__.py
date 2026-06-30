e5f573efde098cde720eecb82dc55e84a3bd4e41
Jens Jørgensen <147208366+J-PJ@users.noreply.github.com>
2026-06-30 13:30:56 +0200

    Update __init__.py

--- gorm_base_control/gorm_base_control/__init__.py
+++ gorm_base_control/gorm_base_control/__init__.py
@@ -1,12 +1,1 @@
-# Publisher for motor telemetry
-self.motor_state_pub = self.create_publisher(
-    Float64MultiArray,
-    '/motor_states',
-    10
-)
 
-# Publish every 100 ms
-self.motor_state_timer = self.create_timer(
-    0.5,
-    self.publish_motor_states
-)
